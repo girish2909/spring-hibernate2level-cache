6d56ad3c51ac8e1779a96cc9b5e6d21c12be4156
girish <girish.mishra1@gmail.com>
2024-07-20 11:21:11 +0530

    commit

---
+++ src/main/java/com/pih/cache/SpringHibernate2levelCacheApplication.java
@@ -0,0 +1,13 @@
+package com.pih.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHibernate2levelCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHibernate2levelCacheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pih/cache/SpringHibernate2levelCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pih.cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHibernate2levelCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
